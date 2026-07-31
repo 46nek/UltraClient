710e2657cb692eb77c131fe37b7e864d1d68018b
yura <nishimoto.yura@gmail.com>
2026-07-31 19:36:47 +0900

    fix: App namespace compilation error

--- src/webview/WebViewHost.cpp
+++ src/webview/WebViewHost.cpp
@@ -264,7 +264,13 @@ void WebViewHost::RegisterEvents() {
                         std::string url = util::WideToUtf8(urlW);
                         ::CoTaskMemFree(urlW);
                         if (url == "https://krunker.io/kuc_ranked.html") {
-                            std::wstring localPath = app::App::GetExeDir() + L"\\ui\\ranked.html";
+                            wchar_t exePath[MAX_PATH] = {};
+                            ::GetModuleFileNameW(nullptr, exePath, MAX_PATH);
+                            std::wstring path(exePath);
+                            size_t lastSep = path.rfind(L'\\');
+                            if (lastSep != std::wstring::npos) path = path.substr(0, lastSep);
+                            
+                            std::wstring localPath = path + L"\\ui\\ranked.html";
                             IStream* stream = nullptr;
                             HRESULT hr = ::SHCreateStreamOnFileEx(localPath.c_str(), 
                                 STGM_READ | STGM_SHARE_DENY_WRITE, 0, FALSE, nullptr, &stream);
